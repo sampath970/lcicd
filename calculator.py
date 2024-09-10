a048e6e70e5f0fbf85fca16b5aca0703bfae0141
Sampath Kumar Naik <sampathkumarnaik4@gmail.com>
2024-09-11 02:03:46 +0530

    add subtraction functionality

--- calculator.py
+++ calculator.py
@@ -2,8 +2,12 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     print(f"The sum is {add(num1, num2)}")
-
+    print(f"The difference is {subtract(num1, num2)}")
